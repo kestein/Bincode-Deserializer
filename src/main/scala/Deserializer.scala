f7cc2cee72474e31c1bc2e240ea7c62107f888e6
kestein <kestein42932@gmail.com>
2018-09-28 15:36:35 -0700

    Implement deserialization for basic number types

---
+++ src/main/scala/Deserializer.scala
@@ -0,0 +1,63 @@
+import java.io.InputStream
+import java.nio.{ByteOrder, ByteBuffer}
+
+class Deserializer(source: InputStream, endianness: ByteOrder = ByteOrder.LITTLE_ENDIAN) {
+  var bytesRead = 0
+  /*
+    Since there are no unsigned types the next size up is used for the corresponding bit size to ensure enough space
+    for all possible values
+  */
+  def deserialize_u8(): Short = {
+    val rawBytes = grabN(1)
+    rawBytes.getShort(0)
+  }
+
+  def deserialize_i8(): Byte = {
+    val rawBytes = grabN(1)
+    rawBytes.get(0)
+  }
+
+  def deserialize_u16(): Int = {
+    val rawBytes = grabN(2)
+    rawBytes.getInt(0)
+  }
+
+  def deserialize_i16(): Short = {
+    val rawBytes = grabN(2)
+    rawBytes.getShort(0)
+  }
+
+  def deserialize_u32(): Long = {
+    val rawBytes = grabN(4)
+    rawBytes.getLong(0)
+  }
+
+  def deserialize_i32(): Int = {
+    val rawBytes = grabN(4)
+    rawBytes.getInt(0)
+  }
+
+  def deserialize_u64(): BigInt = {
+    val rawBytes = grabN(8)
+    BigInt.apply(rawBytes.getLong(0))
+  }
+
+  def deserialize_i64(): Long = {
+    val rawBytes = grabN(8)
+    rawBytes.getLong(0)
+  }
+
+  def grabN(amount: Int): ByteBuffer = {
+    /* Use the next byte size up */
+    val jvmSize = amount*2
+    val buf: Array[Byte] = Array.fill(jvmSize)(0)
+    /* Read the actual requested amount from the underlying buffer */
+    if (source.readNBytes(buf, bytesRead, amount) != amount) {
+      // TODO: do some error here
+      ByteBuffer.allocate(0).order(endianness)
+    } else {
+      bytesRead += amount
+      ByteBuffer.allocate(jvmSize).order(endianness).put(buf)
+    }
+  }
+}
---
+++ src/test/scala/DeserializerTest.scala
@@ -0,0 +1,52 @@
+import java.io.{ByteArrayInputStream, InputStream}
+import java.nio.{ByteBuffer, ByteOrder}
+
+import org.scalatest.FunSuite
+
+class DeserializerTest extends FunSuite {
+  test("Deserializer.deserialize_u8") {
+    val expected = 97
+    val d = makeDeserializer(1, (x:ByteBuffer) => x.put(expected.toByte))
+    assert(d.deserialize_u8() == expected.toShort)
+  }
+  test("Deserializer.deserialize_i8") {
+    val expected = -97
+    val d = makeDeserializer(1, (x:ByteBuffer) => x.put(expected.toByte))
+    assert(d.deserialize_i8() == expected.toByte)
+  }
+  test("Deserializer.deserialize_u16") {
+    val expected = 300
+    val d = makeDeserializer(2, (x:ByteBuffer) => x.putShort(expected.toShort))
+    assert(d.deserialize_u16() == expected.toInt)
+  }
+  test("Deserializer.deserialize_i16") {
+    val expected = -300
+    val d = makeDeserializer(2, (x:ByteBuffer) => x.putShort(expected.toShort))
+    assert(d.deserialize_i16() == expected.toShort)
+  }
+  test("Deserializer.deserialize_u32") {
+    val expected = 300000
+    val d = makeDeserializer(4, (x:ByteBuffer) => x.putInt(expected))
+    assert(d.deserialize_u32() == expected.toLong)
+  }
+  test("Deserializer.deserialize_i32") {
+    val expected = -300000
+    val d = makeDeserializer(4, (x:ByteBuffer) => x.putInt(expected))
+    assert(d.deserialize_i32() == expected)
+  }
+  test("Deserializer.deserialize_u64") {
+    val expected: Long = Long.MaxValue
+    val d = makeDeserializer(8, (x:ByteBuffer) => x.putLong(expected))
+    assert(d.deserialize_u64().toLong == expected)
+  }
+  test("Deserializer.deserialize_i64") {
+    val expected = Long.MinValue
+    val d = makeDeserializer(8, (x:ByteBuffer) => x.putLong(expected))
+    assert(d.deserialize_i64() == expected)
+  }
+
+  def makeDeserializer(capacity: Int, insertVal: ByteBuffer=>ByteBuffer): Deserializer = {
+    val byteRep = ByteBuffer.allocate(capacity).order(ByteOrder.LITTLE_ENDIAN)
+    new Deserializer(new ByteArrayInputStream(insertVal(byteRep).array()))
+  }
+}
