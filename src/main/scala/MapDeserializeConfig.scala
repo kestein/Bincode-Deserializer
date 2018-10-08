3ae9de578d11620c33d24cd13ceb352b6dea1e37
kestein <kestein42932@gmail.com>
2018-10-07 17:17:57 -0700

    Add entry deserialization.

--- src/main/scala/MapDeserializeConfig.scala
+++ src/main/scala/MapDeserializeConfig.scala
@@ -1,4 +1,7 @@
 trait MapDeserializeConfig[K, V] {
   def deserialize_key(d: Deserializer): K
   def deserialize_value(d: Deserializer): V
+  def deserialize_entry(d: Deserializer): (K, V) = {
+    (deserialize_key(d), deserialize_value(d))
+  }
 }
