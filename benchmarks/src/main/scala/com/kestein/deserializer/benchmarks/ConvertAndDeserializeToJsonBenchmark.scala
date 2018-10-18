267824204587fc82a43ccbd8a36e1565021c9110
kestein <kestein42932@gmail.com>
2018-10-18 15:58:13 -0700

    Remove unused benchmark
    Converting cost is much larger than just deserializing the json, so
    measuring it is not worthwhile.

--- benchmarks/src/main/scala/com/kestein/deserializer/benchmarks/ConvertAndDeserializeToJsonBenchmark.scala
+++
@@ -1,7 +0,0 @@
-package com.kestein.deserializer.benchmarks
-
-/* Benchmark of taking the data as a string and loading it as a JsonNode.
- * Does not measure the time it takes to convert the data from bincode to json.*/
-class ConvertAndDeserializeToJsonBenchmark {
-
-}
